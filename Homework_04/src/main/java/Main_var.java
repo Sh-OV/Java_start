ae3444cd97cd8aeeec2838a4e72332386cbd00fc
SH-OV <shchegolga@yandex.ru>
2023-03-26 22:59:20 +0500

    Добавила ветку с вариантом решения через id

--- Homework_04/src/main/java/Main_var.java
+++ Homework_04/src/main/java/Main_var.java
@@ -6,31 +6,32 @@
 4. Два варианта продолжения, либо вывод списка сортированного по возрасту и полу либо завершение приложения.
 Реализовать сортировку, по возможности, с использованием дополнительного индексного списка.
  */
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main_var {
 
-static LinkedList<ArrayList<String>> contact_users = new LinkedList<>();
-static Scanner user_scan = new Scanner(System.in);
+    static LinkedList<ArrayList<String>> contact_users = new LinkedList<>();
+    static Scanner user_scan = new Scanner(System.in);
 
-static LinkedList<Integer> id = new LinkedList<>();
-static int i = 0;
+    static LinkedList<Integer> id = new LinkedList<>();
+    static int i = 1;
+    static HashMap<Integer, ArrayList<String>> hashMap = new HashMap<>();
 
     public static void main(String[] args) {
-        data_entry_conditions(contact_users, id);
-        print_user(contact_users, id);
-        System.out.println("\n Сортировка по возрасту: ");
-        sorting_index(contact_users,3);
-        print_user(contact_users, id);
-        System.out.println("\n Сортировка по полу: ");
-        sorting_index(contact_users,4);
-        print_user(contact_users, id);
+        data_entry_conditions(contact_users);
+        hash_map_users(contact_users, id, hashMap);
+        hashMap.forEach((k, v) -> System.out.println(k + ". " + v));
+        print_user(contact_users, id, hashMap);
+        hashMap.forEach((k, v) -> System.out.println(k + ". " + v));
     }
-    private static ArrayList<String> user_data_entry(LinkedList<Integer> lli) {
-        lli.add(i++);
+
+    private static ArrayList<String> user_data_entry() {
+        id.add(i++);
         ArrayList<String> user = new ArrayList<>();
         System.out.println("Введите Фамилию: ");      // т.к. подключен файл из которого берутся данные, ввод из консоли неактуален
         String last_name = user_scan.nextLine();
@@ -50,23 +51,43 @@ static int i = 0;
         return user;
     }
 
-    private static void print_user(LinkedList<ArrayList<String>> lls, LinkedList<Integer> lli) {
+    private static void hash_map_users(LinkedList<ArrayList<String>> lls,
+                                       LinkedList<Integer> lli,
+                                       HashMap<Integer, ArrayList<String>> hm) {
         for (int j = 0; j < lls.size(); j++) {
-            System.out.print(lli.get(j) + ". ");
-            System.out.print(lls.get(j).get(0) + " ");
-            System.out.print(lls.get(j).get(1).toUpperCase().charAt(0) + ".");
-            System.out.print(lls.get(j).get(2).toUpperCase().charAt(0) + ". ");
-            System.out.print(lls.get(j).get(3) + " ");
-            System.out.println(lls.get(j).get(4));
+            hm.put(lli.get(j), lls.get(j));
         }
     }
 
-    private static void data_entry_conditions(LinkedList<ArrayList<String>> lls, LinkedList<Integer> lli) {
+    private static void print_user(LinkedList<ArrayList<String>> lls,
+                                   LinkedList<Integer> lli,
+                                   HashMap<Integer, ArrayList<String>> hm) {
+        for (int j = 0; j < hm.size(); j++) {
+            ArrayList<String> help = new ArrayList<>();
+            help.add(lls.get(j).get(0) + " " +
+                    lls.get(j).get(1).toUpperCase().charAt(0) + "." +
+                    lls.get(j).get(2).toUpperCase().charAt(0) + ". " +
+                    lls.get(j).get(3) + " " +
+                    lls.get(j).get(4));
+            hm.put(lli.get(j), help);
+
+
+       /*
+            System.out.print(lli.get(j) + ". " +
+                    lls.get(j).get(0) + " " +
+                    lls.get(j).get(1).toUpperCase().charAt(0) + "." +
+                    lls.get(j).get(2).toUpperCase().charAt(0) + ". " +
+                    lls.get(j).get(3) + " " +
+                    lls.get(j).get(4) + "\n");*/
+        }
+    }
+
+    private static void data_entry_conditions(LinkedList<ArrayList<String>> lls) {
         String command;
         System.out.println("Введите 'д' для ввода новой записи и любую клавишу для завершения ввода данных: ");
         command = user_scan.nextLine();
         while (command.equals("д")) {
-            lls.add(user_data_entry(lli));
+            lls.add(user_data_entry());
             System.out.println("Введите 'д' для ввода новой записи и любую клавишу для завершения ввода данных: ");
             command = user_scan.nextLine();
         }
