42667aeafaefe017b222aa44973fd7fe95944058
SH-OV <shchegolga@yandex.ru>
2023-03-27 19:40:36 +0500

    подкорректировала функцию user_data

--- Homework_5/src/main/java/Hw5.java
+++ Homework_5/src/main/java/Hw5.java
@@ -6,27 +6,31 @@ public class Hw5 {
     static ArrayList<String> user = new ArrayList<>();
     static ArrayList<String> number_phone = new ArrayList<>();
     static Scanner user_scan = new Scanner(System.in);
-    static HashMap<ArrayList<String>, ArrayList<String>> users_contacts = new HashMap<>();
+    static HashMap<String, ArrayList<String>> users_contacts = new HashMap<>();
+
     public static void main(String[] args) {
-        user_data(user, number_phone);
+        user_data(users_contacts);
+        users_contacts.forEach((k, v) -> System.out.println(k + ", " + v));
+        System.out.println("=".repeat(30));
     }
 
-    private static void user_data(ArrayList<String> u, ArrayList<String> n, HashMap<ArrayList<String>, ArrayList<String>> uc) {
+    private static void user_data(HashMap<String, ArrayList<String>> uc) {
         String consol;
         System.out.println("Введите 'д' для ввода новой записи и любую клавишу для завершения ввода данных: ");
         consol = user_scan.nextLine();
+        int i = 0;
         while (consol.equals("д")) {
+            ArrayList<String> n = new ArrayList<>();
             System.out.println("Контактное лицо: ");      // т.к. подключен файл из которого берутся данные, ввод из консоли неактуален
             String user = user_scan.nextLine();
-            u.add(user);
-            System.out.println("Номер телефона (11 цифр): ");
+            System.out.println("Номер телефона домашний: ");
             String number_phone = user_scan.nextLine();
             n.add(number_phone);
-            uc.put(u,n);
+            uc.putIfAbsent(user, n);
             System.out.println("Введите 'д' для ввода новой записи и любую клавишу для завершения ввода данных: ");
             consol = user_scan.nextLine();
         }
     }
 
-
+ //   private static void add_number(HashMap<String, ArrayList<String>> uc) {
 }
