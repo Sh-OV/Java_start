2467995b07f7a02140e6cd2d42a048b74e5d0a97
SH-OV <shchegolga@yandex.ru>
2023-03-27 22:26:17 +0500

    Добавила функцию сортировку sorting_popular с помощью TreeMap

--- Homework_5/src/main/java/Hw5.java
+++ Homework_5/src/main/java/Hw5.java
@@ -1,6 +1,14 @@
+/*
+Урок 5. Хранение и обработка данных ч2: множество коллекций Map
+Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов. Пусть дан список сотрудников.
+Напишите программу, которая найдёт и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности имён.
+*Популярность не имён а номеров телефонов
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Hw5 {
     static ArrayList<String> user = new ArrayList<>();
@@ -12,6 +20,7 @@ public class Hw5 {
         user_data(users_contacts);
         users_contacts.forEach((k, v) -> System.out.println(k + ", " + v));
         System.out.println("=".repeat(30));
+        sorting_popular(users_contacts);
     }
 
     private static void user_data(HashMap<String, ArrayList<String>> uc) {
@@ -36,5 +45,10 @@ public class Hw5 {
             System.out.println("Введите 'д' для ввода новой записи и любую клавишу для завершения ввода данных: ");
             consol = user_scan.nextLine();
         }
+    }
+    public static void sorting_popular(HashMap<String, ArrayList<String>> hms){
+        TreeMap<String, ArrayList<String>> t_map = new TreeMap<>();
+        hms.forEach((k,v) -> t_map.put(k,v));
+        t_map.forEach((k, v) -> System.out.println(k + ", " + v));
     }
 }
