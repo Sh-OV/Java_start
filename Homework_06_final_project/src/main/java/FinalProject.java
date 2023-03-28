f06188a0952c0d75412aab62e8c06618e7785c09
SH-OV <shchegolga@yandex.ru>
2023-03-28 11:58:27 +0500

    Homework_6

--- Homework_06_final_project/src/main/java/FinalProject.java
+++ Homework_06_final_project/src/main/java/FinalProject.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /*
 Урок 6. Хранение и обработка данных ч3: множество коллекций Set
 Разработать программу, имитирующую поведение коллекции HashSet.
@@ -7,9 +10,78 @@
 Формат данных Integer.
  */
 public class FinalProject {
-
     public static void main(String[] args) {
-        
+        MySet ms = new MySet();
+        ms.add(1);
+        ms.add(2);
+        ms.add(3);
+        ms.add(1);
+        ms.print();
+        MySet ms1 = new MySet();
+        ms1.add(1);
+        ms1.add(2);
+        ms1.add(5);
+        ms1.add(8);
+        ms1.print();
+        ms.united(ms1);
+        ms.print();
+        ms.delete(3);
+        ms.print();
+        System.out.println(ms.get(2));
+        System.out.println(ms.toArrayKey());
+        System.out.println(ms.toArrayValue(10));
+    }
+}
+
+class MySet {
+    private HashMap<Integer, Object> hm;
+    private static final Object OBJECT = new Object();
+
+    public MySet() {
+        this.hm = new HashMap<>();
+    }
+
+    public void add(Integer num) {
+        this.hm.putIfAbsent(num, OBJECT);
+
+    }
+
+    public void print() {
+        hm.forEach((k, v) -> System.out.print(k));
+        System.out.println();
+    }
+
+    public void united(MySet otherSet) {
+        otherSet.hm.forEach((k, v) -> this.add(k));
+    }
+
+    public void delete(Integer num) {
+        this.hm.remove(num);
+    }
+
+    /*   public void intersect(MySet otherSet){
+           otherSet.hm.
+       }*/
+    public int size() {
+        return this.hm.size();
+    }
+
+    public int get(int index) {
+        return (int) this.hm.keySet().toArray()[index];
+    }
+
+    public ArrayList<Integer> toArrayKey() {
+        return new ArrayList<Integer>(this.hm.keySet());
+    }
+    public ArrayList<Integer> toArrayValue(int element){
+        if(!hm.containsKey(element)) return null;
+        ArrayList<Integer> output = new ArrayList<>();
+        for (int k : hm.keySet()) {
+            output.add(k);
+            if(k == element) break;
+        }
+
+        return output;
     }
 
 }
