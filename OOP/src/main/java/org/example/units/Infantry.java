276f93a6e37606354bed2a12acc16911c011cd60
SH-OV <shchegolga@yandex.ru>
2023-04-15 20:50:46 +0500

    Откорректировала поля абстрактных классов

--- OOP/src/main/java/org/example/units/Infantry.java
+++ OOP/src/main/java/org/example/units/Infantry.java
@@ -1,7 +1,7 @@
 package org.example.units;
 
 public abstract class Infantry extends BaseHero{
-    int gian;     // увеличение удара при разбеге
+    protected  int gian;     // увеличение удара при разбеге
     public Infantry(int x, int y, float hp, int step, int attack, int def, int[] damage,
                     int gian) {
         super(x, y, hp, step, attack, def, damage);
--- OOP/src/main/java/org/example/units/Shooter.java
+++ OOP/src/main/java/org/example/units/Shooter.java
@@ -1,9 +1,9 @@
 package org.example.units;
 
 public abstract class Shooter extends BaseHero {
-    int     arrows,             // количество стрел остаток после каждого удара?
-            maxArrows,          // количество стрел максимально в начале боя
-            accuracy;           // точность
+    protected  int arrows,             // количество стрел остаток после каждого удара?
+                   maxArrows,          // количество стрел максимально в начале боя
+                   accuracy;           // точность
 
     public Shooter(int x, int y, float hp, int step, int attack, int def, int[] damage,
                    int arrows, int accuracy) {
