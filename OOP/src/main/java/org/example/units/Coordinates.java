9334a7631756aba8cdaf646cd85a0c9d30b82076
SH-OV <shchegolga@yandex.ru>
2023-04-17 16:34:09 +0500

    Добавила класс координаты

---
+++ OOP/src/main/java/org/example/units/Coordinates.java
@@ -0,0 +1,22 @@
+package org.example.units;
+
+
+public class Coordinates {
+    protected static int count;
+    protected int x;
+    protected int y;
+    protected float distance;
+
+
+    public Coordinates(int x, int y, float distance, boolean team) {
+        this.x = x;
+        this.y = y;
+        this.distance = distance;
+        count ++;
+    }
+
+    public void coord(){
+
+        if ()
+    }
+}
